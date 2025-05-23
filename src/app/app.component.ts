b7c959ef37b2b8dcd70c7ae42d58d1037bfc7383
Luana <luanabolino.dev@gmail.com>
2025-05-23 17:28:39 -0300

    Atividade 08

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { H1Component } from './components/h1/h1.component';
+import { PComponent } from './components/p/p.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, H1Component, PComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
